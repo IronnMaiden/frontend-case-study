6be7c1bebd1990d27ddfff597d0c95e5b024e5c1
GDdaboss <gddaboss@gmail.com>
2020-10-01 14:53:40 +0530

    Create UselessComponent.jsx

---
+++ UselessComponent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Loginpage from './Loginpage';
+import Calendar from './Calendar';
+import Meetings from './Meetings';
+
+
+function UselessComponent() {
+    return (
+        <div>
+            I do nothing!
+        </div>
+    );
+}
+
+export default UselessComponent;
